c7f752249bdca81f4956271cba49fe1c28b4e0c7
Agustín Nieto <anietog1@eafit.edu.co>
2020-04-04 13:49:26 -0500

    Adding completion functionality and quoting home paths

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 echo "Installing rr"
-install -D -T rr.sh $HOME/.local/bin/rr
+install -D -T rr.sh "$HOME/.local/bin/rr"
 
 echo "Creating profiles folder"
-mkdir -p $HOME/.local/etc/rr
+mkdir -p "$HOME/.local/etc/rr"
 
-echo "Please add $HOME/.local/bin to your PATH"
+echo "Copying completion file into $HOME/.local/etc/bash_completion.d"
+install -D -m 644 rr.bash-completion "$HOME/.local/etc/bash_completion.d"
+
+echo "Add $HOME/.local/bin to your PATH if you want"
+echo "Source $HOME/.local/etc/bash_completion.d/rr.bash-completion if you want"
