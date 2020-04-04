f8bcc1683b90e223aace54d1ffc293cca555b3d1
Agustín Nieto <anietog1@eafit.edu.co>
2020-04-04 15:23:50 -0500

    Copying lib to rr folder

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ mkdir -p "$HOME/.local/etc/rr"
 echo "Copying completion file into $HOME/.local/etc/bash_completion.d"
 install -D -m 644 rr.bash-completion "$HOME/.local/etc/bash_completion.d"
 
+echo "Copying lib into $HOME/.local/etc/rr"
+cp -r lib/* "$HOME/.local/etc/rr"
+
 echo "Add $HOME/.local/bin to your PATH if you want"
 echo "Source $HOME/.local/etc/bash_completion.d/rr.bash-completion if you want"
